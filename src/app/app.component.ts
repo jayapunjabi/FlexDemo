5689b0c4ce939ebe1c12eb0a62b6f1915aeabe65
jayapunjabi <jayapunjabi707@gmail.com>
2020-05-24 14:30:52 +0530

    new updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FlexDemo';
-  index: number = 1;
-  public loop=[ 1,2,3,4,5,6,7,8,9,10];
+  public value;
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
